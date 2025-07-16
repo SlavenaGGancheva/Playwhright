047a78a4814132948c3b85498eb891016df592bc
Slavena Gancheva <slavenaganchevag@gmail.com>
2025-07-16 01:25:36 -0400

    Created POM

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -6,7 +6,6 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Form Layouts page', () => {
     test.beforeEach(async({ page }) => {
-        await page.goto('http://localhost:4200/')
         await page.getByText('Forms').click()
         await page.getByText('Form Layouts').click()
     })
