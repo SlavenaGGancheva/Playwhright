ee72c3f4cd47407bb205b2b85c448df8a006b520
Slavena Gancheva <slavenaganchevag@gmail.com>
2025-07-16 08:23:40 -0400

    Creted a Form Layouts page object

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { NavigationPage } from '../page-objects/navigationPage'
+import { formLayoutsPage } from '../page-objects/formLayoutsPage'
 
 test.beforeEach(async({ page }) => {
     await page.goto('http://localhost:4200')
@@ -12,4 +13,13 @@ test('Navigate to Form page', async({ page }) => {
     await navigateTo.smartTablePage()
     await navigateTo.toastrPage()
     await navigateTo.tooltipPage()
+})
+
+test('Parametrized methods', async ({ page }) => {
+    const navigateTo = new NavigationPage(page)
+    const onFormLayoutsPage = new formLayoutsPage(page)
+
+    await navigateTo.formLayoutsPage()
+    await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
+    await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', true)
 })
